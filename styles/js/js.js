2048a871ad8459b58e9b3c617d0db5f7e6cbbd7a
kautdi <60731005+Vatnik149@users.noreply.github.com>
2022-11-18 22:49:51 +0300

    Add files via upload

---
+++ styles/js/js.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+    // Default parameters
+    slidesPerView: 4,
+    spaceBetween: 30,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    },
+    pagination:{
+      el:".swiper-pagination",
+      clickable:true,
+    },
+    navigation:{
+      nextEl:".swiper-button-next",
+      prevEl:".swiper-button-prev",
+    }
+  })
---
+++ styles/js/map.js
@@ -0,0 +1,24 @@
+ymaps.ready(init); 
+var myMap;
+
+function init() {
+
+    myMap = new ymaps.Map("map", {
+        center: [43.238253, 76.945465], // Координаты центра карты
+        zoom: 13 // Маштаб карты
+    }); 
+
+    myMap.controls.add(
+        new ymaps.control.ZoomControl()  // Добавление элемента управления картой
+    ); 
+
+    myPlacemark = new ymaps.Placemark([43.238253,76.945465], { // Координаты метки объекта
+        balloonContent: "<div class='ya_map'>Заезжайте в гости!</div>" // Подсказка метки
+    }, {
+        preset: "twirl#redDotIcon" // Тип метки
+    });
+    
+    myMap.geoObjects.add(myPlacemark); // Добавление метки
+    myPlacemark.balloon.open(); // Открытие подсказки метки
+    
+};
